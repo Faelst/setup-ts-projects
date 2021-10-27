1ec03c200743d562e4386ba474d24e8429a8a1f9
Rafael Silverio <fael_st@hotmail.com>
2021-10-26 22:43:23 -0300

    config: setup TypeScript projects

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@controllers': './src/controllers',
+          '@lib': './src/lib',
+          '@modules': './src/modules',
+          '@config': './src/config',
+          '@interfaces': './src/interfaces',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts', '**/*.test.ts'],
+};
---
+++ src/__test__/test.spec.ts
@@ -0,0 +1,4 @@
+test('testing jest if running', () => {
+  const a = 1;
+  expect(a).toEqual(1);
+});
